f9feb99b5b0bc1dc60225c1c87bce72672de1842
Javier <jrupmir@epsg.upv.es>
2025-04-07 09:28:06 +0200

    Fix - Slam package

--- src/hispanotech_nav_system/setup.py
+++ src/hispanotech_nav_system/setup.py
@@ -16,7 +16,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'config'), glob('config/*.pgm')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.lua')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
