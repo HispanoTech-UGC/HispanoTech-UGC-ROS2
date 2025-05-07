1c06e84ef362ff84b1fa0df9a3ace33039a753bc
SentoMarcos <smariba@epsg.upv.es>
2025-05-07 11:12:57 +0200

    fix - controles del mando

--- src/hispano_nav_system/hispano_nav_system/ps3_joy_teleop.py
+++ src/hispano_nav_system/hispano_nav_system/ps3_joy_teleop.py
@@ -21,6 +21,34 @@ class JoyVelocityPublisher(Node):
         self.get_logger().info("🎮 JoyDebugger activo — mueve sticks o pulsa botones para ver el mapping")
 
     def joy_callback(self, msg):
+
+        #ejes y botones
+        x_btn = msg.buttons[0]      # boton x
+        o_btn = msg.buttons[1]      # boton o
+        tr_btn = msg.buttons[2]     # boton triángulo
+        sq_btn = msg.buttons[3]     # boton cuadrado
+
+        l1_btn = msg.buttons[4]     # boton L1
+        r1_btn = msg.buttons[5]     # boton R1
+        l2_btn = msg.buttons[6]     # boton L2 
+        r2_btn = msg.buttons[7]     # boton R2
+
+        start_btn = msg.buttons[9]  # boton start
+
+        ji_btn = msg.buttons[11]    # joystick izquiqedo pulsar
+        jd_btn = msg.buttons[12]    # joystick derecho pulsar
+
+        fa_btn = msg.buttons[13]    # flecha arriba
+        fb_btn = msg.buttons[14]    # flecha abajo
+        fi_btn = msg.buttons[15]    # flecha izquierda
+        fd_btn = msg.buttons[16]    # flecha derecha
+       
+        eje_izq_a_b = msg.axes[1]   # eje move izquierdo arriba abajo entre 1 y -1
+        eje_izq_i_d = msg.axes[0]   # eje move izquierdo izquierda derecha 1 y -1 
+        eje_der_a_b = msg.axes[4]   # eje move derecha   arriba abajo entre 1 y -1 
+        eje_der_i_d = msg.axes[3]   # eje move derecha   izquierda derecha 1 y -1 
+
+        '''
         # Mostrar ejes
         for i, axis in enumerate(msg.axes):
             if abs(axis) > 0.1:  # Ignora valores pequeños por ruido
@@ -30,24 +58,19 @@ class JoyVelocityPublisher(Node):
         for i, btn in enumerate(msg.buttons):
             if btn:
                 self.get_logger().info(f"🔘 Botón [{i}] pulsado")
-
-        # Eje R2 analógico — comúnmente en axes[5] (ajusta si es otro)
-        r2_axis_raw = msg.axes[5]  # 1.0 (no pulsado) → -1.0 (pulsado)
-        r2_pressure = max(0.0, (1.0 - r2_axis_raw) / 2.0)  # Normaliza a 0.0 → 1.0 y asegura no valores negativos
+        '''
+        
+        r2_pressure = max(0.0, (1.0 - r2_btn) / 2.0)  # Normaliza a 0.0 → 1.0 y asegura no valores negativos
 
         if r2_pressure <= 0.05:
             # No se mueve si no se pulsa
             self.robot_vel.publish(Twist())
             return
 
-        # Movimiento con stick izquierdo
-        LSTICK_x_axis = msg.axes[0]  # izquierda/derecha
-        LSTICK_y_axis = msg.axes[1]  # adelante/atrás
-
         # Escalar velocidades proporcionalmente a la presión de R2
         cmd_vel = Twist()
-        cmd_vel.linear.x = LSTICK_y_axis * self._linear_scale * r2_pressure
-        cmd_vel.angular.z = LSTICK_x_axis * self._angular_scale * r2_pressure
+        cmd_vel.linear.x = eje_izq_i_d * self._linear_scale * r2_pressure
+        cmd_vel.angular.z = eje_izq_a_b * self._angular_scale * r2_pressure
 
         self.robot_vel.publish(cmd_vel)
 
